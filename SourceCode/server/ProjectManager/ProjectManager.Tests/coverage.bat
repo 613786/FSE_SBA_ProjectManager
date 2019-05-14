631b08c6b869fcdece6d952d2a19e7a337ab265d
613786 <pravin.gupta@cognizant.com>
2019-05-14 05:24:14 -0700

    Code changes

--- SourceCode/server/ProjectManager/ProjectManager.Tests/coverage.bat
+++ SourceCode/server/ProjectManager/ProjectManager.Tests/coverage.bat
@@ -1,2 +1,2 @@
 
-C:\Users\Administrator\Desktop\FSE-ProjectManager\ProjectManager_Server\packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe C:\Users\Administrator\Desktop\FSE-ProjectManager\ProjectManager_Server\ProjectManager.Tests\bin\Debug\ProjectManager.Tests.dll
+C:\PravinGupta_613786\FSE_SBA\SourceCode\server\ProjectManager\packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe C:\PravinGupta_613786\FSE_SBA\SourceCode\server\ProjectManager\ProjectManager.Tests\bin\Debug\ProjectManager.Tests.dll
